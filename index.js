b92108144217c77e2a60d4a8906f8b28291f1296
CarlyS2240 <carly.strathearn@carleton.ca>
2021-10-03 22:06:20 -0400

    Added comment to describe the two different API routes that I created

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.get('/', (req, res) => {
     res.send("Hi from the express server!");
 });
 
+//Created two Api routes, (1) returns 10 facts about me and (2) returns 10 facts about Tokyo
+
 app.get('/api/Facts-About-Me', (req, res) => {
     const factsAboutMe = ["My favourite season is the Fall", "My favourite holiday is Halloween", "My favourite movie genre is horror", "My favourite hobby is making rugs", "I have two cats; Tinkerbell and Thomas", "My favourite food is sushi", "I want to travel to Tokyo someday", "My favourite colour is pink", "My last played song on spotify is Seasons by Mozzy", "My favourite TV show is Black Mirror"];
     res.send(JSON.stringify(factsAboutMe));
